c419086e1cee42bea0c44d58c0dacdb1946a1654
Patrick Peters <45291006+PetersLogistik@users.noreply.github.com>
2024-12-13 13:45:53 +0100

    Update OTVision.cmd

--- OTVision.cmd
+++ OTVision.cmd
@@ -2,7 +2,7 @@
 setlocal
 cd %~dp0\OTVision
 echo start OTVision
-call \venv\Scripts\activate
+call venv\Scripts\activate
 echo Fall 0: python rename.py 
 echo Fall I: python convert.py -p "/to/your/h264 files" 
 echo Fall II: python detect.py -p "/to/video" --expected_duration "video duration [sec]" 
