0dae917179c7fd014761f1bbe45ee0a5592539dd
Patrick Peters <45291006+PetersLogistik@users.noreply.github.com>
2025-06-25 14:45:13 +0200

    new file:   Auswertung_Analytics/Verkehrsbelastung.R 	new file:   Auswertung_Analytics/reduktion_csv_auf_fahrzeuge.py

---
+++ Auswertung_Analytics/Verkehrsbelastung.R
@@ -0,0 +1,57 @@
+#set working directory
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+#load packages
+config_path <- Sys.getenv("R_CONFIG_PATH")
+source(config_path)
+library(ggplot2)
+library(dplyr)
+library(png)
+library(ggpattern)
+library(hrbrthemes)
+library(scales)
+library(lubridate)
+
+file <- "D:/Erhebungen/06-2025 Böhler/Böhler_2025-06-12.counts_15min_reduziert.csv"
+# Read the CSV file into R
+events <- read.csv(file, header = TRUE)
+
+format_hhmm <- function(x) {
+  format(as.POSIXct(x, format="%Y-%m-%d %H:%M:%S"), "%H:%M")
+}
+
+event = events %>%
+  mutate(
+    time = as.POSIXct(start.time, format="%Y-%m-%d %H:%M:%S")
+  ) %>%
+  filter(
+    from.section == "A" | to.section == "A"
+  ) %>%
+  group_by(
+    classification, time
+  ) %>%
+  summarise(
+    anz = sum(count, na.rm = TRUE),
+    .groups = "drop"
+  ) 
+
+d1 <- event %>%
+  ggplot(aes(x=time, y=anz, color=classification))+
+  geom_point()+
+  geom_line()+
+  #geom_smooth(method="loess")+
+  scale_x_continuous(labels = format_hhmm,)+
+  labs(title = "Digitale Verkehrsauswertung - Ein und Ausfahrten Areal Böjler", 
+       subtitle = "03.06.2025",
+       caption = paste("n =",format( nrow(event), big.mark = ".", decimal.mark = ",", scientific = FALSE)),
+       x = "Uhrzeit",
+       y = "Anzahl",
+       color = "Farzeugart") +
+  theme_light()+
+  theme(legend.position = "bottom")
+
+d1
+
+out <- "D:/Erhebungen/06-2025 Böhler/Digitale_Verkehrsauswertung_12-06-2025_2.png"
+png(filename=paste0(out), width = 21, height = 14.8, res = 600, units = 'cm')#A5
+d1
+dev.off()
---
+++ Auswertung_Analytics/reduktion_csv_auf_fahrzeuge.py
@@ -0,0 +1,28 @@
+# UTF-8
+import pandas as pd
+
+def reduktion_df(file_path, sep=',') -> None:
+    typ = ('bicycle', 'bus', 'car', 'motorcycle', 'person', 'train', 'truck')
+    df = pd.read_csv(file_path, sep=sep, header=0, encoding='utf-8')
+    df_reduziert = df[df['classification'].isin(typ)]
+    df = df[~df['classification'].isin(typ)]
+    print(f"Es wurden {len(df_reduziert)} gefunden. {len(df)} Kategorien wurden aufgrund einer nicht passende Kategorie reduziert. Darin sind {df['count'].sum()} Werte, die das System erkannt hat.")
+    df_reduziert.to_csv(file_path.replace('.csv','_reduziert.csv', 1), index=False)
+
+def zusammenfassen() -> object:
+    s1 = r"D:\Erhebungen\06-2025 Böhler\02-06_06-00\Böhler02_6_00_2025-06-20_13-33-11.counts_15min.csv"
+    df1 = pd.read_csv(s1, sep=',', header=0, encoding='utf-8')
+    s2 = r"D:\Erhebungen\06-2025 Böhler\02-06_14-00\Böhler_2025-06-25_12-20-21.counts_15min.csv"
+    df2 = pd.read_csv(s2, sep=',', header=0, encoding='utf-8')
+    df3 = pd.concat([df1, df2], axis=0, ignore_index=True)
+    df3.to_csv(r"D:\Erhebungen\06-2025 Böhler\Böhler02_06_2025.csv", index=False)
+
+if __name__ == "__main__":
+    # zusammenfassen()
+    reduktion_df(r"D:\Erhebungen\06-2025 Böhler\Böhler_2025-06-12.counts_15min.csv")
+    # path = input('Dateipfad bitte Angeben: ')
+    # try:
+    #     reduktion_df(path)
+    # except Exception as e:
+    #     print(f'Fehler: {e}')
+    
