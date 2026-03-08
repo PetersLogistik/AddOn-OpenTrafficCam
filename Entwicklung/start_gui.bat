1aeac1a67acb03bfe807eba800a6d6903e0d05d2
Patrick Peters <45291006+PetersLogistik@users.noreply.github.com>
2026-03-08 16:44:27 +0100

    new file:   Entwicklung/start_gui.bat

---
+++ Entwicklung/start_gui.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Aktiviere virtuelles Py-Umfeld
+if not exist ".env\Scripts\activate.bat" (
+    echo Virtuelles Python-Umfeld nicht gefunden.
+    python -m venv .env
+    call .env\Scripts\activate
+    python.exe -m pip install --upgrade pip
+    pip install -r guiBueffee/requirements.txt --no-cache-dir
+    echo "Installation abgeschlossen"
+)
+
+call .env\Scripts\activate
+:: führe Skript aus
+call python "%~dp0guiBueffee/gui_helper.py"
+:: Beende alles
+deactivate
+::wenn deactivate nicht funktioniert
+endlocal
+:: Console schließen
+exit
